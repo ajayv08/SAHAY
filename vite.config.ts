a26f1ec741d92deecbe677384527a1c6a2c8ead4
ajayv08 <ajayvishwakarma050307@gmail.com>
2026-09-05 12:32:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/SAMAY/',
+    base: '/SAHAY/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
